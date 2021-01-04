af4516224ec31c280e704537259a990153bf1d40
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-04 20:02:00 +0000

    assertEqual assignment added

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("⭐Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❗Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(5, 1);
+assertEqual("Lighthouse Labs", "lighthouse labs");
+assertEqual(1, "boo");
