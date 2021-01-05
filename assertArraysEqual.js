6b7f525fcdb0abd3c3e8ccb2061c78dd62298607
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-05 19:46:40 +0000

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+  // Check for array sizes, they must be the same size to be considered equal.
+  if (arr1.length === arr2.length) {
+    let matches = 0;
+    //iterate over first and second array and compare contents
+    for (let i = 0; i < arr1.length; i++) {
+
+      if (arr1[i] === arr2[i]) {
+        matches ++;
+      }
+    }
+    //If the number of matching elements is the same as the length of the array. Then they are equal.
+    if (matches === arr1.length) {
+      return true;
+    }
+    
+  }
+  return false;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`⭐Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❗Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//test code
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
