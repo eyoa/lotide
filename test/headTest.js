c1303aae33c76871459ad600df03cbcb31bde97d
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-11 18:17:39 +0000

    setup mocha and chai for testing. converted headTest to use M&C

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,28 @@
 //test code
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 7);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  
+  it("returns 5 for [5, 6, 7]", () => {
+    const array = [5,6,7];
+    assert.strictEqual(head(array), 5);
+  });
+  
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const array = ['Hello', 'Lighthouse', 'Labs'];
+    assert.strictEqual(head(array), 'Hello');
+  });
+
+  it("returns undefined if passed empty array", () => {
+    const array = [];
+    assert.strictEqual(head(array), undefined);
+  });
+  
+});
